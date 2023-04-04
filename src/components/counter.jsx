4f624af4c3f5390a78e2b76c7cd8682b29e191ad
Lana <tiushkievich00@mail.ru>
2023-04-04 13:32:03 +0200

    feat: add image

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Counter = () => {
+  const count = 0;
+  const formatCount = () => {
+    return count === 0 ? <h1>empty</h1> : count;
+  };
+  const imageUrl = "https://picsum.photos/200";
   return (
     <>
-      <h1>Counter</h1>
+      <img src={imageUrl} alt="" />
+      <span>{formatCount()}</span>
       <button>+</button>
     </>
   );
