f4fe2ae9100a341626d0edeada568b37e29843e3
Lana <tiushkievich00@mail.ru>
2023-04-04 17:20:41 +0200

    feat: add simple conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
   const formatCount = () => {
     return count === 0 ? "empty" : count;
@@ -20,6 +21,29 @@ const Counter = () => {
   const handleDecrement = () => {
     setCount((prevState) => prevState - 1);
   };
+
+  const handleTagChange = (id) => {
+    // setTags(["tag4", "tag5"]);
+    setTags((prevState) => prevState.filter((tag) => tag !== id));
+    // console.log(id);
+  };
+
+  const renderTags = () => {
+    if (tags.length === 0) return "No Tags";
+    return tags.map((tag) => (
+      <li
+        key={tag}
+        className="btn btn-primary btn-sm m-2"
+        onClick={() => handleTagChange(tag)}
+      >
+        {tag}
+      </li>
+    ));
+  };
+
+  if (tags.length !== 0) {
+    return <ul>{renderTags()}</ul>;
+  }
 
   return (
     <>
