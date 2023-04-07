f30e299675b0bfe874fd72b4299ce449c3818fcd
Lana <tiushkievich00@mail.ru>
2023-04-07 20:20:39 +0200

    feat: add countersList component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
 
   const formatCount = () => {
     return count === 0 ? "empty" : count;
@@ -22,31 +21,8 @@ const Counter = () => {
     setCount((prevState) => prevState - 1);
   };
 
-  const handleTagChange = (id) => {
-    // setTags(["tag4", "tag5"]);
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-    // console.log(id);
-  };
-
-  const renderTags = () => {
-    if (tags.length === 0) return "No Tags";
-    return tags.map((tag) => (
-      <li
-        key={tag}
-        className="btn btn-primary btn-sm m-2"
-        onClick={() => handleTagChange(tag)}
-      >
-        {tag}
-      </li>
-    ));
-  };
-
-  if (tags.length !== 0) {
-    return <ul>{renderTags()}</ul>;
-  }
-
   return (
-    <>
+    <div>
       <span className={getBageClasses()}>{formatCount()}</span>
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
@@ -54,7 +30,7 @@ const Counter = () => {
       <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
         -
       </button>
-    </>
+    </div>
   );
 };
 
---
+++ src/components/countersList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+  const counters = [
+    { id: 0, value: 0 },
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+  ];
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter key={count.id} />
+      ))}
+    </>
+  );
+};
+
+export default CountersList;
